c7bfbb3b5f2a0c34b1a5bc99ecd76e312c99198a
Oguz Ozgen <oguzozgen@outlook.com>
2019-04-26 12:32:22 +0300

    custompage fixed

--- ClientApp/src/Router/Routes.js
+++ ClientApp/src/Router/Routes.js
@@ -4,14 +4,11 @@ import { withOidcSecure } from "@axa-fr/react-oidc-context";
 import Home from "../pages/Home/Home";
 import Dashboard from "../pages/Dashboard/Dashboard";
 import Admin from "../pages/Admin/Admin";
-import Profile from "../BaseT-React-PagesCustom/Profile/Profile";
-
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route path="/dashboard" component={withOidcSecure(Dashboard)} />
     <Route path="/admin" component={Admin} />
-    <Route path="/profile" component={Profile}/>
   </Switch>
 );
 
